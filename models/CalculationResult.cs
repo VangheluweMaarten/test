66e94d2c7049c332a5f1051a406a416e454ca60f
VangheluweMaarten <maarten.vangheluwe@student.howest.be>
2023-11-26 12:39:28 +0100

    commit

--- models/CalculationResult.cs
+++
@@ -1,9 +0,0 @@
-using Newtonsoft.Json;
-namespace mct.models;
-public class CalculationResult
-{
-    [JsonProperty("result")]
-    public int Result { get; set; }
-    [JsonProperty("operator")]
-    public string Operator { get; set; }
-}
